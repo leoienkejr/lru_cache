24725400ab52d6b284e1da2d2b3794c4ee6794a3
leoienkejr <leonardoienkejr@gmail.com>
2022-08-12 02:54:47 +0000

    Add test for LinkedList.is_empty()

--- lru_cache/tests/test_linked_list.py
+++ lru_cache/tests/test_linked_list.py
@@ -4,4 +4,14 @@ from lru_cache.linked_list import LinkedList
 def test_insert():
     ll = LinkedList()
     ll.insert(5)
-    assert ll._head is not None and ll._head.data == 5
+    ll_head = getattr(ll, '_head')
+    assert ll_head is not None and ll_head.data == 5
+    assert ll_head.list_ref == ll
+
+
+def test_is_empty():
+    ll = LinkedList()
+    assert ll.is_empty()
+
+    ll.insert(4)
+    assert not ll.is_empty()
