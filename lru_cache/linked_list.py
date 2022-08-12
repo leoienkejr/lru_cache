4f824647ea0a2deb291326b944801799cd94993c
leoienkejr <leonardoienkejr@gmail.com>
2022-08-12 02:54:20 +0000

    Change attribute names in LinkedListNode

--- lru_cache/linked_list.py
+++ lru_cache/linked_list.py
@@ -2,18 +2,18 @@ from typing import Any, Optional
 
 
 class LinkedListNode:
-    _list_ref: Optional['LinkedList']
-    _previous: Optional['LinkedListNode']
-    _next: Optional['LinkedListNode']
+    list_ref: Optional['LinkedList']
+    previous: Optional['LinkedListNode']
+    next: Optional['LinkedListNode']
     data: Any = None
 
     def __init__(self, data: Any):
-        self._previous = None
-        self._next = None
+        self.previous = None
+        self.next = None
         self.data = data
 
     def is_middle_node(self) -> bool:
-        return (self._previous is not None) and (self._next is not None)
+        return (self.previous is not None) and (self.next is not None)
 
 
 class LinkedList:
@@ -28,6 +28,9 @@ class LinkedList:
     def is_empty(self) -> bool:
         return (self._head is None) and (self._tail is None)
 
+    def contains(self, node: LinkedListNode) -> bool:
+        return node.list_ref is self
+
     def insert(self, data: Any):
         """ Insert new node containing 'data'
         at the head of the linked list
@@ -36,14 +39,14 @@ class LinkedList:
         """
 
         node = LinkedListNode(data)
-        node._list_ref = self
+        node.list_ref = self
 
         if self.is_empty():
             self._head = node
             self._tail = self._head
         else:
-            node._next = self._head
-            self._head._previous = node  # type: ignore
+            node.next = self._head
+            self._head.previous = node  # type: ignore
             self._head = node
 
         self.length += 1
@@ -52,22 +55,22 @@ class LinkedList:
         if node is self._head:
             return
         if node.is_middle_node():
-            previous = node._previous
-            next = node._next
+            previous = node.previous
+            next = node.next
 
-            previous._next = next  # type: ignore
-            next._previous = previous  # type: ignore
+            previous.next = next  # type: ignore
+            next.previous = previous  # type: ignore
 
-            node._previous = None
-            node._next = self._head
+            node.previous = None
+            node.next = self._head
 
             self._head = node
         else:
-            previous = node._previous
-            previous._next = None  # type: ignore
+            previous = node.previous
+            previous.next = None  # type: ignore
 
             self._tail = previous
 
-            node._next = self._head
-            node._previous = None
+            node.next = self._head
+            node.previous = None
             self._head = node
